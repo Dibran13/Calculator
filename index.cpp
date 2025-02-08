2f6ecad71a2c7eb5f8157f81e1f4d634dbf085ad
AgonHoxha <agon.hoxha28@gmail.com>
2025-02-09 00:37:03 +0100

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -8,3 +8,13 @@ int main() {
   return 0;
 }
 
+double orePune, pagaPerOre, pagaBruto, tatimi, pagaNeto, bonusi = 0;
+    const double normaTatimit = 0.15; // 15% tatim
+    const double bonusPerOre = 10.0; // Bonus për çdo orë mbi 40
+    
+    cout << "=====================================" << endl;
+    cout << "        KALKULATORI I PAGES         " << endl;
+    cout << "=====================================" << endl;
+    
+    cout << "Jepni numrin e oreve te punes: ";
+    cin >> orePune;
