8f28ff18ee5d934aee8dda6b63e0a41f67e6297e
AgonHoxha <agon.hoxha28@gmail.com>
2025-02-09 00:39:18 +0100

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -4,11 +4,8 @@
 using namespace std;
 
 int main() {
-
-  return 0;
-}
-
-double orePune, pagaPerOre, pagaBruto, tatimi, pagaNeto, bonusi = 0;
+ 
+  double orePune, pagaPerOre, pagaBruto, tatimi, pagaNeto, bonusi = 0;
     const double normaTatimit = 0.15; // 15% tatim
     const double bonusPerOre = 10.0; // Bonus për çdo orë mbi 40
     
@@ -18,3 +15,8 @@ double orePune, pagaPerOre, pagaBruto, tatimi, pagaNeto, bonusi = 0;
     
     cout << "Jepni numrin e oreve te punes: ";
     cin >> orePune;
+
+  return 0;
+}
+
+
